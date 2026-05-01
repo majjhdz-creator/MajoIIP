8139cd2a110b0675c2d93edadcd86fa3621d2911
majjhdz-creator <majjhdz@gmail.com>
2026-04-30 20:23:58 -0600

    Add files via upload

---
+++ #Variables No-Numéricas.py
@@ -0,0 +1,8 @@
+#Variables No-Numéricas
+
+nombre_de_usuario = input ("nombre: ")
+cantidad_de_pizzas = int (float ("cantidad de pizzas: "))
+esta_pagado = input ("¿Esta pagado?") ("Yes/no: ")
+direccion_del_envio = input ("Direccion: ")
+precio_total = int (float ("total: "))
+

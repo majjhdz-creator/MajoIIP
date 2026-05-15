954964eb8a5a4e775cb6a5248036175093d00302
majjhdz-creator <majjhdz@gmail.com>
2026-05-14 20:37:27 -0600

    Add files via upload

---
+++ #Ordenar estudiantes por nombre.py
@@ -0,0 +1,21 @@
+#Ordenar estudiantes por nombre
+#María José
+
+"nombre" = nombre
+"edad" = edad
+
+#crear 5 estudiantes
+
+e1 = {"Nombre": victoria, "edad": 16}
+e2 = {"Nombre": Sofia , "edad": 17}
+e3 = {"Nombre": Ximena , "edad": 23}
+e4 = {"Nombre": Dafne , "edad": 18}
+e5 = {"Nombre": Nahomi , "edad": 12}
+
+#Guardar en una lista = 
+[e1, e2, e3, e4, e5]
+
+#Ordena lista usando el nombre
+{"Ordenar lista de estudiantes: e1, e2, e3, e4, e5"}
+
+("Mostrar resultado en: , resultado")
